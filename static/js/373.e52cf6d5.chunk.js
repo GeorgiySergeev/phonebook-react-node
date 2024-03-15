31abe416aaa68cb6de30b0bc2f62a7ccb781452f
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2024-03-15 11:55:29 +0000

    Deploying to gh-pages from @ GeorgiySergeev/phonebook-react-node@2dd5559490471b5b032592d8fe186a0cbdaa82b6 🚀

---
+++ static/js/373.e52cf6d5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkphonebook_react_node=self.webpackChunkphonebook_react_node||[]).push([[373],{4373:function(t,e,n){n.d(e,{hjJ:function(){return o}});var c=n(9983);function o(t){return(0,c.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M294.1 256L167 129c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.3 34 0L345 239c9.1 9.1 9.3 23.7.7 33.1L201.1 417c-4.7 4.7-10.9 7-17 7s-12.3-2.3-17-7c-9.4-9.4-9.4-24.6 0-33.9l127-127.1z"}}]})(t)}}}]);
+//# sourceMappingURL=373.e52cf6d5.chunk.js.map
